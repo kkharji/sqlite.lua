aae8feac8338584bea7047a11c0f22c210fbf963
Tami <65782666+tami5@users.noreply.github.com>
2021-08-31 13:16:44 +0300

    :zap: gen_rockspec

--- scripts/gen_rockspec.lua
+++ scripts/gen_rockspec.lua
@@ -7,7 +7,7 @@ local c = function(l)
   return table.concat(l, ",\n")
 end
 
-local version = ""
+local version = uv.os_getenv "GTAG"
 local modules = {}
 local description = {
   summary = "SQLite/LuaJIT binding and a highly opinionated wrapper for storing, retrieving, caching, and persisting [SQLite] databases",
@@ -18,7 +18,7 @@ local description = {
 }
 
 local dependencies = {
-  "lua >= 5.1",
+  "luv",
 }
 
 --- Format Dependencies -----------------------------------
@@ -32,25 +32,7 @@ for _, v in ipairs(scandir(cwd, { search_pattern = "/lua/sqlite/[^examples]" }))
   local path = v:gsub(cwd, "")
   local module = path:gsub("/", "%."):gsub(".lua.(.-).lua", "%1")
   modules[module] = path
-  -- modules[#modules + 1] = ('  ["%s"] = "%s"'):format(module, path)
 end
--- modules = c(modules)
-
---- Get last version tag ----------------------------------
-job
-  :new({ -- git name-rev --tags --name-only $(git rev-parse HEAD)
-    "git",
-    "describe",
-    "--tags",
-    "--abbrev=0",
-    on_exit = function(j, c)
-      if c ~= 0 then
-        os.exit(1, true)
-      end
-      version = j:result()[1]
-    end,
-  })
-  :sync()
 
 local output = ([[
 rockspec_format = "3.0"
